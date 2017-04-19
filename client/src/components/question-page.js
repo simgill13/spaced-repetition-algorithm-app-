9d4f4f7e29f4972cb1a73f76bdc73fbd8fe6551f
Mousecop <joaskew13@gmail.com>
2017-04-19 11:47:39 -0400

    cleans up code and whitespace

--- client/src/components/question-page.js
+++ client/src/components/question-page.js
@@ -3,39 +3,27 @@ import {connect} from 'react-redux';
 import * as Cookies from 'js-cookie';
 import {gettingQuestions} from '../actions/action';
 
-
-
  class QuestionPage extends React.Component {
     constructor(props) {
         super(props);
-         this.componentWillMount=this.componentWillMount.bind(this);
-    }
-
-
-
-    componentWillMount() {
-        const accessToken = Cookies.get('accessToken');
-        // this.props.getQuestions(accessToken);   
     }
 
     render() {
         let s = this.props.questions[0].spanish
         console.log(s)
 
-
         return (
-                    <div>
-                        <div className="questions">
-                            <p>Display word here</p>
-                        </div>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <div className="answer">
-                             <p>answer here </p>
-                        </div>
-                    </div>
-            
+            <div>
+                <div className="questions">
+                    <p>Display word here</p>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <div className="answer">
+                    <p>answer here </p>
+                </div>
+            </div>
         );
     }
 }
@@ -45,10 +33,4 @@ const mapStateToProps = (state) => ({
 
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    getQuestions(accessToken) {
-        dispatch(gettingQuestions(accessToken));
-    }
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(QuestionPage);
+export default connect(mapStateToProps)(QuestionPage);
