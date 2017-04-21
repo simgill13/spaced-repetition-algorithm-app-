39d330d84e3c69330b54c528dbe7ef63d7dd0d41
Mousecop <joaskew13@gmail.com>
2017-04-21 14:59:39 -0400

    adds constructor and classname

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -3,11 +3,14 @@ import {connect} from 'react-redux';
 import $ from 'jquery';
 
 class Nav extends React.Component {
+	constructor(props) {
+		super(props);
+	}
     render() { 
         return (
         	<div className="Nav">        		
         		<div className="name">
-        			<h1> {this.props.displayName} </h1>
+        			<h1 className="title"> {this.props.displayName} </h1>
         			 <a className="logout" href={'/api/auth/logout'}>logout</a>
         		</div>
         	</div>
@@ -17,7 +20,7 @@ class Nav extends React.Component {
    
 const mapStateToProps = (state) => ({
 	displayName: state.displayName,
-	currentUser: state.currentUser
+	currentUser: state.currentUser,
 });
 
 export default connect(mapStateToProps)(Nav);
